8dde16c72264adab8927fa967922930bbbf795de
Ascellan <mikorkea@student.jyu.fi>
2021-05-10 09:48:11 +0300

    Adding python code

---
+++ madlibsgen.py
@@ -0,0 +1,57 @@
+game = "Lost in the Apocalypse"
+print(game)
+print("Welcome my friend!")
+
+question1 = input("It seems like you are feeling adventurous today, am I right?: ")
+if question1 == "yes" or question1 == "Yes":
+    print("I knew it!")
+    print("Try to keep your pants on, pal, because you're about to enter the exciting world of Apocalyptic Madness!")
+    print("This is where everything is upside down and laughter is guaranteed.")
+    print("Now let's throw in some words.")
+elif question1 == "no" or question1 == "No":
+    print("Too bad! You can always come back later. Have a nice day!")
+    exit()
+
+else:
+    print("You are adorable. There's nothing to be afraid of!")
+    print("Now let's throw in some words.")
+
+#asking for an input from user
+input1 = input("Give me a plural substantive: ")
+input2 = input("Adjective, please: ")
+input3 = input("Substantive, please: ")
+input4 = input("A plural substantive: ")
+input5 = input("Again, a plural substantive: ")
+input6 = input("One more a plural substantive!: ")
+input7 = input("A substantive: ")
+input8 = input("Aaannd the last one, a plural substantive: ")
+
+print("Thank you! Good job!")
+print("You've got quite an imagination! I'm exited to see what kind of world you've created!")
+
+question3 = input("Are you ready to see what your Apocalyptic Madness is like?: ")
+if question3 == "yes" or question3 == "Yes":
+    print("Alright, here comes the Madness!")
+
+else:
+    print("Now come on, I know you want to!")
+    print("Here comes the Madness, ready or not!")
+
+#
+sentence1 = f"As I walked along the {input1} of the last of my kind"
+sentence2 = f"I imagined my mind being the most {input2} tool at the moment"
+sentence3 = f"because I was in the {input3} in the middle of the night."
+sentence4 = f"Swirls of dust escaped the gravity when the {input4} haunted me with the most tortorous voices"
+sentence5 = f"I was a giant amongst {input5}, consuming their essence to the bone."
+sentence6 = f"Hill after hill, mountain after another those fallen {input6} soared the skies and painted the space with their gruesome figure."
+sentence7 = f"Piece by piece the ingenious strategy of the {input7} revealed itself for me."
+sentence8 = f"Its purpose wasn't just to tremble the very core of the weakest {input8} that still ventured to fathom the Earth but to gain leverage."
+
+print(sentence1 + " ")
+print(sentence2 + " ")
+print(sentence3 + " ")
+print(sentence4 + " ")
+print(sentence5 + " ")
+print(sentence6 + " ")
+print(sentence7 + " ")
+print(sentence8)
